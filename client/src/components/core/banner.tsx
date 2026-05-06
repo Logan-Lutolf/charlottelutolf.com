b3745958bcc04825c3cdd49f2712412a79e1b83b
Logan Lutolf <loganlutolf@gmail.com>
2026-05-06 14:23:40 -0400

    added banner (wip)

---
+++ client/src/components/core/banner.tsx
@@ -0,0 +1,12 @@
+
+
+
+export function Banner () {
+    return (
+        <>
+            <section id={"banner"} className={"bg-secondary-foreground h-96"}>
+
+            </section>
+        </>
+    );
+}
