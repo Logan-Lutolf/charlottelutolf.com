f2fb25905d194a08b6c8360036383777eed7a4c5
Logan Lutolf <loganlutolf@gmail.com>
2026-05-07 14:50:28 -0400

    download resume button

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import {Banner} from "../components/core/banner.tsx";
 import {SkillCard} from "../components/skill-card.tsx";
 import {Link} from "react-router-dom";
+import {Button} from "../components/ui/button.tsx";
 
 
 export function Home () {
@@ -36,22 +37,29 @@ export function Home () {
 
             <Banner />
             <section id={"about-section"} className={"flex flex-row mt-5 px-5"}>
-                <div className="flex flex-col items-center text-wrap max-w-lg mr-5">
-                    <h1 className={"text-2xl underline underline-offset-5 mb-3"}>About Me</h1>
-                    <p>I'm Charlotte, a Junior CS student at Worcester Polytechnic Institute.</p>
-                    <p>Check out my skills and projects!</p>
+                <div className="flex flex-col justify-evenly items-center">
 
-                    <div id={"quick-links-container"} className={"flex flex-row items-center justify-evenly gap-4 mt-5"}>
-                        <Link to={"https://github.com/Logan-Lutolf"}>
-                            <img src={"../../public/github-logo.png"} width={35}/>
-                        </Link>
-                        <Link to={"https://www.linkedin.com/in/logan-lutolf"}>
-                            <img src={"../../public/linkedin-logo.png"} width={35}/>
-                        </Link>
-                        <Link to={"mailto:loganlutolf@gmail.com"}>
-                            <img src={"../../public/gmail-logo.png"} width={43}/>
-                        </Link>
+                    <div className="flex flex-col items-center text-wrap max-w-lg mr-5">
+                        <h1 className={"text-2xl underline underline-offset-5 mb-3"}>About Me</h1>
+                        <p>I'm Charlotte, a Junior CS student at Worcester Polytechnic Institute.</p>
+                        <p>Check out my skills and projects!</p>
+
+                        <div id={"quick-links-container"} className={"flex flex-row items-center justify-evenly gap-4 mt-5"}>
+                            <Link to={"https://github.com/Logan-Lutolf"}>
+                                <img src={"../../public/github-logo.png"} width={35}/>
+                            </Link>
+                            <Link to={"https://www.linkedin.com/in/logan-lutolf"}>
+                                <img src={"../../public/linkedin-logo.png"} width={35}/>
+                            </Link>
+                            <Link to={"mailto:loganlutolf@gmail.com"}>
+                                <img src={"../../public/gmail-logo.png"} width={43}/>
+                            </Link>
+                        </div>
                     </div>
+
+                    <Button className={"p-5 text-lg"}>
+                        <a href={"/public/LoganLutolfResume.pdf"} download={"Logan Lutolf Resume"}>Download My Resume!</a>
+                    </Button>
 
                 </div>
                 <div className="flex flex-row items-center gap-2 flex-1">
