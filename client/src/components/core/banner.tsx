927da6e0698569a6026cd04069ffff67bfcbe3dd
Logan Lutolf <loganlutolf@gmail.com>
2026-05-07 12:59:30 -0400

    placeholder banner animation

--- client/src/components/core/banner.tsx
+++ client/src/components/core/banner.tsx
@@ -3,10 +3,10 @@ import '../../animations/banner-animation.css';
 export function Banner () {
     return (
         <>
-            <section id={"banner"} className={"bg-secondary-foreground h-96"}>
-                <ul id={"animation-container"} className={"circles"}>
+            <section id={"banner"} className={"relative bg-secondary-foreground h-96 z-0"}>
+                <ul id={"animation-container h-full"} className={"circles"}>
                     {Array.from({length: 10}).map((_, i) => (
-                        <li key={i} className={"shape"}></li>
+                        <li key={i}></li>
                     ))}
                 </ul>
             </section>
