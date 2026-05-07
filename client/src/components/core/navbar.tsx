2235517815c4e128cb2f626ad479411f656ac3f7
Logan Lutolf <loganlutolf@gmail.com>
2026-05-07 11:31:59 -0400

    navbar links

--- client/src/components/core/navbar.tsx
+++ client/src/components/core/navbar.tsx
@@ -1,18 +1,21 @@
+import {Link} from "react-router-dom";
 
 
 export function Navbar () {
     return (
         <>
             <section id={"header"}
-                     className={"flex flex-row justify-between items-center bg-primary h-16 px-5 text-background font-semibold text-white"}>
+                     className={"flex flex-row justify-between items-center bg-primary " +
+                         "h-16 px-5 text-background font-semibold text-white text-xl"}>
                 <div>
                     Charlotte Lutolf
                 </div>
 
                 <div id={"nav-links"} className={"flex flex-row justify-evenly gap-10"}>
-                    <a>Home</a>
+                    <Link to={"/"}> Home </Link>
                     <a>About</a>
-                    <a>Projects</a>
+                    <Link to={"/projects"}>Projects</Link>
+                    <Link to={"/testing"}>Testing</Link>
                 </div>
             </section>
         </>
