13d1cda5551023a7a52ac144b9e58ca92283e33a
Logan Lutolf <loganlutolf@gmail.com>
2026-05-07 13:42:36 -0400

    finished skill card

--- client/src/components/skill-card.tsx
+++ client/src/components/skill-card.tsx
@@ -1,22 +1,28 @@
 import {Card, CardContent, CardHeader} from "./ui/card.tsx";
 
 
-export function SkillCard (){
-    const skills = [
-        'Object-Oriented Programming',
-        'Systems Programming',
-        'Algorithm Development',
-        'Database Management',
-        'Agile & Scrum Methodologies'];
+type skillCardProps = {
+    skills: string[];
+    title: string;
+}
 
+export function SkillCard ({ skills, title } : skillCardProps) {
     return (
         <>
-            <Card className={"bg-primary"}>
-                <CardHeader>Skills</CardHeader>
-                <CardContent>
+            <div className={"w-1/5 flex flex-col pl-2 bg-"}>
 
-                </CardContent>
-            </Card>
+                <Card className={"bg-secondary-foreground flex flex-col gap-3 border border-accent max-h-96"}>
+                    <CardHeader className={"text-background text-lg"}>{title}</CardHeader>
+                    <CardContent className={"flex flex-col gap-2 text-background overflow-y-scroll"}>
+                        {skills.map((skill) => (
+                            <div className={"bg-secondary border-accent flex flex-row justify-start p-3 rounded-2xl"}>
+                                {skill}
+                            </div>
+                        ))}
+                    </CardContent>
+                </Card>
+            </div>
+
         </>
     );
 }
